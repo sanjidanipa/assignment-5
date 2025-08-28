51b011583473f2ce9a93bda617592c55a6cc0bb3
Sanjida Akter Nipa <sanjidanipa2004@gmail.com>
2025-08-28 21:25:33 +0600

    btn

--- index.js
+++ index.js
@@ -1,6 +1,68 @@
-const historyData = [];
 
 
-document.getElementById("national-emergency-call").addEventListener("click", function() {
-    console.log("clicked btn")
-})
+const nationalCall = document.getElementById("national_emergency_call");
+
+const policeCall = document.getElementById ("police_helpline_call");
+
+const fireCall = document.getElementById("fire_service_call");
+
+const ambulanceCall = document.getElementById("ambulance_service_call");
+
+const womenCall = document.getElementById("women_child_help_call");
+
+const antiCorruptionCall = document.getElementById("anti_corruption_call");
+
+ const electricityCall = document.getElementById("electricity_helpline_call");
+
+const bracCall = document.getElementById ("brac_helpline_call");
+
+const railwayCall = document.getElementById ("railway_helpline_call");
+const na
+
+
+
+
+
+
+//   nationalButton.addEventListener("click", function(){
+//     const serviceName = document.getElementById("service_name1").innerText;
+//     const serviceNum = document.getElementById("service_num1").innerText;
+//     console.log(serviceName , serviceNum)
+//       alert( serviceName , serviceNum)
+// })
+
+//  policeButton.addEventListener("click", function(){
+//     console.log("clicked")
+
+//  })
+
+//  fireButton.addEventListener("click", function(){
+//   console.log("clicked")
+//  })
+
+//  ambulanceButton.addEventListener("click", function(){
+//   console.log("clicked")
+//  })
+
+//  womenButton.addEventListener("click", function(){
+//   console.log("clicked")
+//  })
+
+//   antiCorruptionButton.addEventListener("click", function(){
+//   console.log("clicked")
+//  })
+
+ 
+//  electricityButton.addEventListener("click", function(){
+//   console.log("clicked")
+//  })
+
+ 
+//  bracButton.addEventListener("click", function(){
+//   console.log("clicked")
+//  })
+
+
+//  railwayButton.addEventListener("click", function(){
+//   console.log("clicked")
+//  })
