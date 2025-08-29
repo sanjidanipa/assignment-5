314a363582b645f4555129e490b38200bcdb9771
Sanjida Akter Nipa <sanjidanipa2004@gmail.com>
2025-08-29 22:26:13 +0600

    all question

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const callHistory = []
+let callHistory = []
 const coin = document.getElementById("current_coin");
 const heart = document.getElementById("love_add");
 const copy = document.getElementById("copy_num");
@@ -33,6 +33,33 @@ const now = new Date();
 
   callHistory.push(history);
 }
+
+
+function callHistoryData(){
+  const callHistoryContainer = document.getElementById("history_container");
+   callHistoryContainer.innerText = "";
+   for(const history of callHistory){
+    const newElement = document.createElement("div")
+    newElement.innerHTML= `
+    <div class="bg-[#FAFAFA] rounded-xl p-4 flex justify-between items-center my-5">
+
+            <div>
+              <h1 class="font-semibold text-lg text-[#111111] ">
+              ${history.name}
+              </h1>
+              <p class="text-lg text-[#5C5C5C] ">
+              ${history.num}
+              </p>
+            </div>
+
+        <p class="text-lg text-[#111111] ">${history.time}</p>
+        </div> `
+
+        callHistoryContainer.appendChild(newElement);
+   }
+
+}
+// call button
 function call(nameId, numId) {
   const serviceName = document.getElementById(nameId).innerText;
   const serviceNum = document.getElementById(numId).innerText;
@@ -47,33 +74,18 @@ function call(nameId, numId) {
     alert("NOt Sufficient Coin");
   }
 
-  //  const callHistoryContainer = document.getElementById("history_container");
-  //  callHistoryContainer.innerText = "";
-  //  for(const history of callHistoryAdd){
-  //   const newElement = document.createElement("div")
-  //   newElement.innerHtml = `
-  //   <div class="bg-[#FAFAFA] rounded-xl p-4 flex justify-between items-center my-5">
-
-  //           <div>
-  //             <h1 class="font-semibold text-lg text-[#111111] ">
-  //             ${history.name}
-  //             </h1>
-  //             <p class="text-lg text-[#5C5C5C] ">
-  //             999
-  //             </p>
-  //           </div>
-
-  //       <p class="text-lg text-[#111111] ">${history.time}</p>
-  //       </div> `
-  //       callHistoryContainer.appendChild(newElement);
-  //  }
-
-  //  const currentCoin = parseInt(coin.innerText)-20
-  //   coin.innerText = currentCoin;
-
-
-
+  callHistoryData()
 }
+
+
+// clear button--
+const clearButton = document.getElementById("clear_btn")
+clearButton.addEventListener("click", function(){
+  callHistory = [] ;
+  const callHistoryContainer = document.getElementById("history_container");
+   callHistoryContainer.innerHTML = "";
+
+})
 
 
 // natinal-----
